c1a3caa6aff72216b3c2115da1b65e709240a37a
Tom ODay <tomoday91@gmail.com>
2020-09-08 17:17:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index(1) do |dwarf,index|
+  array.each_with_index do |dwarf,index|
     puts "index. #{dwarf}"
   end
 end
